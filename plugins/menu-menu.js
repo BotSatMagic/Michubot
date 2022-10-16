b79992dc91003415f45c7c82d340e134316981b3
BotSatMagic <victor-albert2022@outlook.com>
2022-10-15 23:35:18 -0500

    yo

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -441,15 +441,7 @@ _(𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)_
 ° ඬ⃟👑 _${usedPrefix}addcmd *<texto> <responder a sticker/imagen>*_
 ° ඬ⃟👑 _${usedPrefix}delcmd *<responder a sticker/imagen con comando o texto asignado>*_
 
-*<𝕄𝔸𝕋𝔸 𝕎ℍ𝔸𝕋𝕊𝔸ℙℙ - 𝕍𝕀ℝ𝕌𝕊/>*
 
-° ඬ⃟☠ _${usedPrefix}crash𝟷_
-° ඬ⃟☠ _${usedPrefix}crash2_
-° ඬ⃟☠ _${usedPrefix}crash3_
-° ඬ⃟☠ _${usedPrefix}crash4_
-° ඬ⃟☠ _${usedPrefix}crash5_
-° ඬ⃟☠ _${usedPrefix}crash6_
-° ඬ⃟☠ _${usedPrefix}crash7_
 `.trim()
 let buttons = [
 { buttonId: '#estado', buttonText: { displayText: '📮 HOLA 📮' }, type: 1 },
